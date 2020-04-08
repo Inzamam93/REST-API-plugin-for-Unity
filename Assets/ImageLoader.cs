d10c85696be1d676aeffca35ecd44fcac008e5a5
inzamam ghazzali <inzamam.gh@live.com>
2020-04-08 18:02:26 +0530

    Rest endpoint calls test

---
+++ Assets/ImageLoader.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class ImageLoader : MonoBehaviour
+{
+    //public string url = "https://i.pinimg.com/originals/9e/1d/d6/9e1dd6458c89b03c506b384f537423d9.jpg";
+    //public string url = "https://randomuser.me/api/?gender=female";
+    public string url = "https://pokeapi.co/api/v2/pokemon/1";
+    public Renderer ImageRenderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadFromPokeApi());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void processJsonData( string url_)
+    {
+        JsonData jsonData_ = JsonUtility.FromJson<JsonData>(url_);
+        Debug.Log(jsonData_.info);
+        
+    }
+
+    IEnumerator LoadFromRestEndPoint()
+    {
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(url))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                // Get downloaded asset bundle
+                //var texture = DownloadHandlerTexture.GetContent(uwr);
+                ImageRenderer.material.mainTexture = DownloadHandlerTexture.GetContent(uwr);
+                processJsonData(url);
+            }
+        }
+    }
+
+    IEnumerator LoadFromPokeApi()
+    {
+        UnityWebRequest pokeContentRequest_ = UnityWebRequest.Get(url);
+        yield return pokeContentRequest_.SendWebRequest();
+
+        if (pokeContentRequest_.isNetworkError || pokeContentRequest_.isHttpError)
+        {
+            Debug.LogError(pokeContentRequest_.error);
+            yield break;
+        }
+
+        JSONNode pokeImageJsonData_ = JSON.Parse(pokeContentRequest_.downloadHandler.text);
+        string pokeSpriteURL_ = pokeImageJsonData_["sprites"]["front_default"];
+
+        // Get Pokemon Sprite
+        UnityWebRequest pokeSpriteRequest_ = UnityWebRequestTexture.GetTexture(pokeSpriteURL_);
+
+        yield return pokeSpriteRequest_.SendWebRequest();
+
+        if (pokeSpriteRequest_.isNetworkError || pokeSpriteRequest_.isHttpError)
+        {
+            Debug.LogError(pokeSpriteRequest_.error);
+            yield break;
+        }
+
+        ImageRenderer.material.mainTexture = DownloadHandlerTexture.GetContent(pokeSpriteRequest_);
+    }
+}
+
---
+++ Assets/JsonData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class JsonData
+{
+    public List<ResultsList> results;
+    public List<InfoList> info;
+}
+
+public class ResultsList
+{
+    public string gender;
+    public List<string> name;
+    public List<string> location;
+    public string email;
+    public List<string> login;
+    public List<string> dob;
+    public string registered;
+    public string phone;
+    public string cell;
+    public List<string> id;
+    public List<string> picture;
+    public string nat;
+
+}
+
+public class InfoList
+{
+    public string seed;
+    public int results;
+    public int page;
+    public float version; 
+}
